8e2bc53ce903a9295a2d8cd16b77f881a52044de
Admir Osmanovic <adosm10@student.sdu.dk>
2020-05-04 15:02:24 +0200

    Item class
    functionality added
    
    Co-Authored-By: babuskah <babuskah@users.noreply.github.com>
    Co-Authored-By: dafar18 <dafar18@users.noreply.github.com>

--- AsteroidsNetbeansModules/CommonItem/src/main/java/dk/sdu/mmmi/cbse/commonitem/ItemType.java
+++ AsteroidsNetbeansModules/CommonItem/src/main/java/dk/sdu/mmmi/cbse/commonitem/ItemType.java
@@ -5,24 +5,30 @@
  */
 package dk.sdu.mmmi.cbse.commonitem;
 
+import java.util.Random;
+
 /**
  *
  * @author maimartinsen
  */
 public enum ItemType {
-    
-    ENLARGE_PLAYER(1),
-    ENLARGE_BULLET(2),
-    ENLARGE_ENEMY(3);
-    
-    
+
+    ENLARGE_PLAYER(0),
+    ENLARGE_BULLET(1),
+    ENLARGE_ENEMY(2);
+
     private int type;
-    
+
     private ItemType(int type) {
         this.type = type;
     }
 
     int getType() {
         return type;
+    }
+    
+    public static ItemType getRandomType() {
+        Random random = new Random();
+        return values()[random.nextInt(values().length)];
     }
 }
--- AsteroidsNetbeansModules/Item/src/main/java/dk/sdu/mmmi/cbse/item/ItemPlugin.java
+++ AsteroidsNetbeansModules/Item/src/main/java/dk/sdu/mmmi/cbse/item/ItemPlugin.java
@@ -5,26 +5,59 @@
  */
 package dk.sdu.mmmi.cbse.item;
 
+import dk.sdu.mmmi.cbse.common.data.Entity;
 import dk.sdu.mmmi.cbse.common.data.GameData;
 import dk.sdu.mmmi.cbse.common.data.World;
+import dk.sdu.mmmi.cbse.common.data.entityparts.PositionPart;
 import dk.sdu.mmmi.cbse.common.services.IGamePluginService;
+import dk.sdu.mmmi.cbse.commonitem.Item;
+import dk.sdu.mmmi.cbse.commonitem.ItemType;
+import org.openide.util.lookup.ServiceProvider;
+import org.openide.util.lookup.ServiceProviders;
 
 /**
  *
  * @author maimartinsen
  */
+@ServiceProviders(value = {
+    @ServiceProvider(service = IGamePluginService.class),})
 public class ItemPlugin implements IGamePluginService {
+    
+    private Entity item;
 
     @Override
     public void start(GameData gameData, World world) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        item = createItem(gameData);
+        world.addEntity(item);
     }
 
     @Override
     public void stop(GameData gameData, World world) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        for (Entity e : world.getEntities(Item.class)) {
+            world.removeEntity(e);
+        }
     }
     
-    private 
+    private Item createItem(GameData gameData) {
+        
+        float radians = 3.1415f / 2 + (float) Math.random();
+        float x = gameData.getDisplayWidth() / 2 + 100;
+        float y = gameData.getDisplayHeight() / 2 + 50;
+
+        float[] colour = new float[4];
+        colour[0] = 0.2f;
+        colour[1] = 1.0f;
+        colour[2] = 0.07f;
+        colour[3] = 1.0f;
+        
+        Entity itemType = new Item(ItemType.getRandomType());
+        itemType.add(new PositionPart(x, y, radians));
+        itemType.setColour(colour);
+        itemType.setRadius(10);
+        
+        return (Item) itemType;
+    }
+    
+    
     
 }
