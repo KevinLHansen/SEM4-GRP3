e98dc0537ff149f0ab4a21cd8a30906d2a6d8eda
KevinLHansen <kevin.lhansen@hotmail.com>
2020-05-08 13:32:07 +0200

    Revert "Revert "Merge branch 'master' into items""
    This reverts commit 91a901108af659e1bc16e6d341853b1b350405fc.

--- AsteroidsNetbeansModules/CommonAsteroids/src/main/java/dk/sdu/mmmi/cbse/common/asteroids/IAsteroidSplitter.java
+++
@@ -1,8 +0,0 @@
-package dk.sdu.mmmi.cbse.common.asteroids;
-
-import dk.sdu.mmmi.cbse.common.data.Entity;
-
-public interface IAsteroidSplitter {
-
-    Entity createSplitAsteroid(Entity e);
-}
--- AsteroidsNetbeansModules/Core/src/main/java/dk/sdu/mmmi/cbse/core/main/Installer.java
+++ AsteroidsNetbeansModules/Core/src/main/java/dk/sdu/mmmi/cbse/core/main/Installer.java
@@ -14,15 +14,15 @@ public class Installer extends ModuleInstall {
         game = new Game();
 
         LwjglApplicationConfiguration cfg = new LwjglApplicationConfiguration();
-        cfg.title = "SEM4 GRP3";
-        cfg.width = 1500;
-        cfg.height = 800;
+        cfg.title = "Pepega Hunter 2020";
+        cfg.width = 1280;
+        cfg.height = 720;
         
         cfg.backgroundFPS = 60;
         cfg.foregroundFPS = 60;
         
         cfg.useGL30 = false;
-        cfg.resizable = false;
+        cfg.resizable = true;
 
         new LwjglApplication(game, cfg);
     }
