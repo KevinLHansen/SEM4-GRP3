aa125e165d4a4db3ea8268c3d3cb5a2ffd239552
IIsSchulstad <sjoeholm.rs@gmail.com>
2020-05-12 13:52:07 +0200

    Changed walls from b2d

--- AsteroidsNetbeansModules/MapLoader/src/main/java/dk/sdu/mmmi/cbse/maploader/TileLoader.java
+++ AsteroidsNetbeansModules/MapLoader/src/main/java/dk/sdu/mmmi/cbse/maploader/TileLoader.java
@@ -20,6 +20,8 @@ import com.badlogic.gdx.physics.box2d.Box2DDebugRenderer;
 import com.badlogic.gdx.physics.box2d.FixtureDef;
 import com.badlogic.gdx.physics.box2d.PolygonShape;
 import com.badlogic.gdx.physics.box2d.World;
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  *
@@ -36,10 +38,11 @@ public class TileLoader {
     public void load(String mapPath) {
         tiledMap = mapLoader.load(mapPath);
         mapRenderer = new OrthogonalTiledMapRenderer(tiledMap);
+        
         this.createWalls(tiledMap);
     }
     
-    private void createWalls(TiledMap tiledMap){
+    private List<Rectangle> createWalls(TiledMap tiledMap){
         String layerName = "walls";
         
         MapLayer layer = tiledMap.getLayers().get(layerName);
@@ -49,24 +52,14 @@ public class TileLoader {
         
         System.out.println(b2dWorld.toString() + "\n\n\n\n\n\n\n\n\n\n");
         
-        BodyDef bdef = new BodyDef();
-        PolygonShape shape = new PolygonShape();
-        FixtureDef fdef = new FixtureDef();
-        Body body;
+        List<Rectangle> walls = new ArrayList<Rectangle>();
         
         for (MapObject object : layer.getObjects().getByType(RectangleMapObject.class)) {
             Rectangle rect = ((RectangleMapObject) object).getRectangle();
-            
-            bdef.type = BodyDef.BodyType.StaticBody;
-            bdef.position.set(rect.getX() + rect.getWidth() / 2, rect.getY() + rect.getHeight() / 2);
-            
-            body = b2dWorld.createBody(bdef);
-            
-            shape.setAsBox(rect.getWidth() / 2, rect.getHeight() / 2);
-            fdef.shape = shape;
-            body.createFixture(fdef);
-            
+            walls.add(rect);
         }
+        
+        return walls;
     }
     
     public World getWorld(){
