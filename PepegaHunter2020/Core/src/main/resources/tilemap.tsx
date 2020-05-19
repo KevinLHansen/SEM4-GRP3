8aff5e17732559f88413f22836e37b9ad2863aa8
KevinLHansen <kevin.lhansen@hotmail.com>
2020-05-19 23:15:21 +0200

    Remove incorrectly placed assets

--- PepegaHunter2020/Core/src/main/resources/tilemap.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.2" tiledversion="1.3.4" name="sewer_1" tilewidth="16" tileheight="16" tilecount="352" columns="16">
- <image source="sewer_1.png" width="256" height="352"/>
-</tileset>
